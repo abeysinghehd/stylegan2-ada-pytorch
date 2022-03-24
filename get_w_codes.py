e3d2cf13e14c92601ccaf8eeb18bb8beab11b65b
Hashan <abeysinghe.hd@gmail.com>
2022-03-24 23:50:00 +0800

    functionality to save weights to a csv file

--- get_w_codes.py
+++ get_w_codes.py
@@ -9,6 +9,7 @@ import dnnlib
 import numpy as np
 import PIL.Image
 import torch
+import csv
 
 import legacy
 
@@ -31,13 +32,13 @@ def num_range(s: str) -> List[int]:
 @click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
 @click.option('--seeds', type=num_range, help='List of random seeds')
 @click.option('--trunc', 'truncation_psi', type=float, help='Truncation psi', default=1, show_default=True)
-# @click.option('--outdir', help='Where to save the output images', type=str, required=True, metavar='DIR')
+@click.option('--outfilecsv', help='Where to save the w values csv', type=str, required=True, metavar='DIR')
 def generate_w_values(
     ctx: click.Context,
     network_pkl: str,
     seeds: Optional[List[int]],
     truncation_psi: float,
-    # outdir: str,
+    outfilecsv: str,
 ):
 
     print('Loading networks from "%s"...' % network_pkl)
@@ -52,12 +53,18 @@ def generate_w_values(
         
     # Labels.
     label = torch.zeros([1, G.c_dim], device=device)
+    
+    with open(outfilecsv, 'w') as f:
+        writer = csv.writer(f)
 
     # Generate images.
     for seed_idx, seed in enumerate(seeds):
         
         z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
         ws = G.mapping(z, label, truncation_psi=truncation_psi)
+        unique_weights = ws[0][0].numpy()
+        csv_row = ','.join([str(seed)] + [str(i) for i in unique_weights])
+        writer.writerow(csv_row)
         print('Generated ws value for seed %d (' % (seed))
         print(f"ws Tensor: \n {ws} \n")
 
