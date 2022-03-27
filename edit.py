b2b04cab502f36c1b2dc73eb391cb8a45704cc6f
Hashan <abeysinghe.hd@gmail.com>
2022-03-28 06:58:59 +0800

    error fix

--- edit.py
+++ edit.py
@@ -100,9 +100,9 @@ def linear_interpolate_images(boundry_path, output_dir, network_pkl, steps):
   # else:
   print(f'  Sample latent codes randomly.')
   # latent_codes = model.easy_sample(args.num, **kwargs)
-  latent_codes = np.random.randn(10, 14, 512)
-  latent_codes = latent_codes.reshape(-1, 14, 512)
-  latent_codes = latent_codes.astype(np.float32)
+  # latent_codes = np.random.randn(10, 14, 512)
+  # latent_codes = latent_codes.reshape(-1, 14, 512)
+  # latent_codes = latent_codes.astype(np.float32)
   
   # Generate images.
   latent_codes = []
@@ -111,8 +111,10 @@ def linear_interpolate_images(boundry_path, output_dir, network_pkl, steps):
   for seed_idx, seed in enumerate(seeds):
       z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
       ws = G.mapping(z, label, truncation_psi=0.7)
+      ws = ws.to('cpu').numpy()
       latent_codes.append(ws)
-  latent_codes = np.array(latent_codes.to('cpu').detach().numpy())
+  latent_codes = np.array(latent_codes)
+  latent_codes = latent_codes.reshape(-1, 14, 512)
   latent_codes = latent_codes.astype(np.float32)
 
   np.save(os.path.join(output_dir, 'latent_codes.npy'), latent_codes)
